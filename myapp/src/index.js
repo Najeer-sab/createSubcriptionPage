e472c8bb7323fd9c617e0b2f2c5ed2e9f2fc9c83
Najeer-sab <najeersab2000@gmail.com>
2023-12-09 00:31:39 +0530

    Modified Responsive Page

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Pricing from './pricing/paricingpage'
 
-
-import Pricing from './Subcription/pricing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
